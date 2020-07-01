4f3bfbd5ccbf98c3fd10400112db6d48dc6d6947
David Du <davdu21@bergen.org>
2020-07-01 17:59:54 -0400

    new image subcode-> subcode2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 
 
-    var hs = [];
+    /*var hs = [];
     function setHeight(){
         var x = 32;
         var y = 35;
@@ -33,13 +33,7 @@ $(document).ready(function() {
             $(".projects").css("font-size", x - 3);
             $("h2").css("font-size", x + 5);
             hs.push(h);
-            $("#down_arrow").css({
-                "width": y,
-                "height": y,
-                "margin":0,
-                "position":"relative",
-                "top": y
-            });
+
 
         }
         else{
@@ -54,14 +48,9 @@ $(document).ready(function() {
             $(".projects").css("font-size", 20);
             $("h2").css("font-size", "2em");
             $(".carousel").css("padding-top", 50);
-            $("#down_arrow").css({
-                "width": 75,
-                "height": 75,
-                "position":"relative",
-                "top": 0
-            })
+
         }
-    }
+    }*/
 
     function recall_menu(){
         if($(window).width() <= 768){
@@ -116,69 +105,8 @@ $(document).ready(function() {
                 window.location.hash = hash;
             });
         }
-    })
+    });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*$(this).scrollTop(0);
-    var scrolltimer, scrolltimer2;
-    window.addEventListener('scroll', function(){
-        clearTimeout(scrolltimer);
-        clearTimeout(scrolltimer2);
-
-        if(document.body.className.indexOf('disable-hover') == -1) {
-            document.body.className += ' disable-hover';
-        }
-
-        scrolltimer = setTimeout(function(){
-            var classes = document.body.className.split(" ");
-            for(var i = 0; i<classes.length; i++){
-                if( classes[i] == 'disable-hover' )
-                    classes.splice(i,1);
-            }
-            document.body.className = classes.join(" ");
-        },200);
-
-        scrolltimer2 = setTimeout(function(){
-            if ($(window).scrollTop() > 0) {
-                $('#n').addClass('sticky');
-            } else {
-                $('#n').removeClass('sticky');
-            }
-        },200);
-    }, false);
-
-    (function($) {
-        $.fn.goTo = function() {
-            $('html, body').animate({
-                scrollTop: ( $(this).offset().top - 30) + 'px'
-            }, 2000);
-            return this;
-        }
-    })(jQuery);
-
-    $('#n').find("a[href*=\\#]").click(function(e){
-        e.preventDefault();
-        var target = $(this).attr('href');
-        $(target).goTo();
-    });*/
 });
